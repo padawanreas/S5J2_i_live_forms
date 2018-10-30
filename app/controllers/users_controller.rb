0411e10cc6498c0fa1526c1ea4c6f82350cfda81
Maxime-Beaufils <thelimier@gmail.com>
2018-10-30 16:55:14 +0100

    add form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
+
+  def index
+    @user=User.all
+  end
+
   def new
-    @user = User.all
+    @user=User.new
   end
 
   def create
-    @user = User.all
+=begin
+    @user = User.create('username' => params[:username],
+                        'email' => params[:email],
+                        'bio' => params[:bio])
+=end
+    @user = User.create('username' => params.require(:user)[:username],
+                    'email' => params.require(:user)[:email],
+                    'bio' => params.require(:user)[:bio])
+    redirect_to users_path
   end
+
 end
